7f83eb919f32e9588893362e3d62efc11db25a87
Нуржан <100024122+lightSoulDev@users.noreply.github.com>
2022-03-01 01:03:27 +0600

    feat: base svg path animation

--- App.tsx
+++ App.tsx
@@ -1,20 +1,35 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import React, { useState } from "react";
+import { SafeAreaView, StyleSheet, Text, View } from "react-native";
+import SvgCheckbox from "./src/SvgCheckbox";
 
 export default function App() {
+  const [checked, setCheked] = useState(true);
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
+    <SafeAreaView style={styles.container}>
+      <SvgCheckbox checked={checked} color="#2c2c2c" />
+      <Text
+        style={styles.check}
+        onPress={() => {
+          setCheked(!checked);
+        }}
+      >
+        CHECK
+      </Text>
+      <View style={{ height: 100 }}></View>
       <StatusBar style="auto" />
-    </View>
+    </SafeAreaView>
   );
 }
 
 const styles = StyleSheet.create({
+  check: {
+    fontSize: 24,
+  },
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
---
+++ src/AnimatedLine.tsx
@@ -0,0 +1,33 @@
+import React, { useRef, useState } from "react";
+import Animated, { useAnimatedProps } from "react-native-reanimated";
+import { Path, PathProps } from "react-native-svg";
+
+const AnimatedPath = Animated.createAnimatedComponent(Path);
+
+interface AnimatedSvgProps extends PathProps {
+  progress: Animated.SharedValue<number>;
+}
+
+const AnimatedSvg = ({ progress, ...pathProps }: AnimatedSvgProps) => {
+  const [length, setLength] = useState(0);
+  const ref = useRef<typeof AnimatedPath>(null);
+  // https://github.com/software-mansion/react-native-reanimated/issues/2766
+  // beizer func issue with Typescript
+  const animatedProps = useAnimatedProps(() => ({
+    strokeDashoffset: Math.max(0, length - length * progress.value),
+  }));
+
+  return (
+    <AnimatedPath
+      animatedProps={animatedProps}
+      // @ts-ignore
+      onLayout={() => setLength(ref.current!.getTotalLength())}
+      // @ts-ignore
+      ref={ref}
+      strokeDasharray={length}
+      {...pathProps}
+    />
+  );
+};
+
+export default AnimatedSvg;
